d11921a8010a847dea69a1e52ac77a6688b45fda
Shaksi <iam@shak.si>
2017-08-20 02:27:33 +0100

    code cleanup and fixed jwt callbacks - now generates token

--- src/Plugin/rest/resource/TokenRestResource.php
+++ src/Plugin/rest/resource/TokenRestResource.php
@@ -2,8 +2,15 @@
 
 namespace Drupal\json_web_token\Plugin\rest\resource;
 
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\jwt\Transcoder\JwtTranscoderInterface;
+use Drupal\jwt\Authentication\Event\JwtAuthGenerateEvent;
+use Drupal\jwt\Authentication\Event\JwtAuthEvents;
+use Drupal\jwt\JsonWebToken\JsonWebToken;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
 * Provides a resource to get a JWT token.
@@ -19,13 +26,39 @@ use Drupal\rest\ResourceResponse;
 */
 
 class TokenRestResource extends ResourceBase {
-  public function post() {
+    /**
+     * The JWT Transcoder service.
+     *
+     * @var \Drupal\jwt\Transcoder\JwtTranscoderInterface
+     */
+    protected $transcoder;
 
-    if($this->currentUser->isAnonymous()){
+    /**
+     * The event dispatcher.
+     *
+     * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * Constructs a HTTP basic authentication provider object.
+     *
+     * @param \Drupal\jwt\Transcoder\JwtTranscoderInterface $transcoder
+     *   The jwt transcoder service.
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
+     *   The event dispatcher service.
+     */
+    public function __construct() {
+      $this->transcoder = JwtTranscoderInterface;
+      $this->eventDispatcher = EventDispatcherInterface;
+    }
+
+  public function post() {
+    if(\Drupal::currentUser()->isAnonymous()){
      $data['message'] = $this->t("Login failed. If you don't have an account register. If you forgot your credentials please reset your password.");
     }else{
      $data['message'] = $this->t('Login succeeded');
-     $data['token'] = $this->generateToken();
+  //   $data['token'] = $this->generateToken();
     }
 
     return new ResourceResponse($data);
@@ -34,13 +67,11 @@ class TokenRestResource extends ResourceBase {
   /**
   * Generates a new JWT.
   */
-  protected function generateToken() {
-    $token = new JsonWebToken();
-    $event = new JwtAuthIssuerEvent($token);
-    $this->eventDispatcher->dispatch(JwtAuthIssuerEvents::GENERATE, $event);
+  public function generateToken() {
+    $event = new JwtAuthGenerateEvent(new JsonWebToken());
+    $this->eventDispatcher->dispatch(JwtAuthEvents::GENERATE, $event);
     $jwt = $event->getToken();
-
-    return $this->transcoder->encode($jwt, array());
+    return $this->transcoder->encode($jwt);
   }
 
 }
